ac509ad2e0263bc633a4a9d9bc72c065e8dbec7f
Michael <mioncgp@gmail.com>
2020-10-17 10:27:42 +0400

    d

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Sign Out/i);
+  const linkElement = getByText(/Register/i);
   expect(linkElement).toBeInTheDocument();
 });
